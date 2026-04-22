2418600b3a56973c1fc1f799babb8f1e77312703
luciangrigor <luciangrigorasuk@gmail.com>
2026-04-22 14:15:07 +0100

    Final Version 1.0.0
    Deployable version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ export default function RootLayout() {
   return (
     <BleProvider>
       <Stack>
+        <Stack.Screen name="index"           options={{ headerShown: false }} />
         <Stack.Screen name="(auth)"         options={{ headerShown: false }} />
         <Stack.Screen name="onboarding"    options={{ headerShown: false, animation: 'slide_from_bottom', gestureEnabled: false }} />
         <Stack.Screen name="(tabs)"        options={{ headerShown: false, animation: 'slide_from_bottom' }} />
